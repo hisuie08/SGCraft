c0551956f533e6d93869f1309a3de6de14e18545
Dockter <dockter@almuramc.com>
2020-07-21 20:52:13 -0500

    Fix for: https://github.com/AlmuraDev/SGCraft/issues/157
    Signed-off-by: Dockter <dockter@almuramc.com>

--- src/mod/gcewing/sg/block/DHDBlock.java
+++ src/mod/gcewing/sg/block/DHDBlock.java
@@ -127,21 +127,23 @@ public class DHDBlock extends BaseBlock<DHDTE> {
         if (localDHD != null) {
             if (localDHD.isLinkedToStargate) {
                 SGBaseTE localGate = localDHD.getLinkedStargateTE();
-                if (cy <= 0.5) {
-                    if (player != null && localGate.allowAdminAccess(player.getName())) {
-                        if (!world.isRemote) {
-                            SGCraft.mod.openGui(player, SGGui.DHDFuel, world, pos);
-                            return true;
-                        }
-                    }
-                } else {
-                    if (world.isRemote) { // Only call on the client, server call here not needed because it doesn't have a container.
-                        if (player != null && localGate.allowGateAccess(player.getName())) {
-                            SGCraft.mod.openGui(player, SGGui.SGController, world, pos);
-                            return true;
+                if (localGate != null) {
+                    if (cy <= 0.5) {
+                        if (player != null && localGate.allowAdminAccess(player.getName())) {
+                            if (!world.isRemote) {
+                                SGCraft.mod.openGui(player, SGGui.DHDFuel, world, pos);
+                                return true;
+                            }
                         }
                     } else {
-                        return true; // Server expects return true here if client was successful, which we can assume it was.
+                        if (world.isRemote) { // Only call on the client, server call here not needed because it doesn't have a container.
+                            if (player != null && localGate.allowGateAccess(player.getName())) {
+                                SGCraft.mod.openGui(player, SGGui.SGController, world, pos);
+                                return true;
+                            }
+                        } else {
+                            return true; // Server expects return true here if client was successful, which we can assume it was.
+                        }
                     }
                 }
             }
